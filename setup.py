7a40c2f3cd37194c9c7c22b8ddea87d24ba76641
AhmedBoujaada <50082927+AhmedBoujaada@users.noreply.github.com>
2021-07-15 12:12:23 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='topk_mallows',
-    version='0.0.1',    
+    version='0.1.0',    
     description='A python package to for Mallows Model with top-$k$ and complete rankingsu using both Kendall and Hamming distance.',
     url='https://github.com/AhmedBoujaada/topk_mallows',
-    author='Ahmed Boujaada and Ekhine Irurozki,
+    author='Ahmed Boujaada and Ekhine Irurozki',
     author_email='aboujaada@bcamath.org',
     license='',
     keywords=['Mallows models', 'Top-k rankings', 'Permutations', 'Kendall\'s tau distance', 'Hamming distance', 'Rankings', 'Complete rankings'],
